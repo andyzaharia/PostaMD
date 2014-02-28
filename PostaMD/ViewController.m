da0ce0e11275fd1c6bd05182110c12fe05ceb0c3
Andrei Zaharia <andyzaharia@gmail.com>
2014-02-28 22:36:52 +0200

    Initial Commit

---
+++ PostaMD/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PostaMD
+//
+//  Created by Andrei Zaharia on 2/28/14.
+//  Copyright (c) 2014 Andrei Zaharia. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ PostaMD/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PostaMD
+//
+//  Created by Andrei Zaharia on 2/28/14.
+//  Copyright (c) 2014 Andrei Zaharia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
