d266571658760fe56d079e64b8d59a74f615a3b4
Andrei <andyzaharia@gmail.com>
2014-02-28 22:36:52 +0200

    Initial Commit

---
+++ PostaMD/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PostaMD
+//
+//  Created by Andrei Zaharia on 2/28/14.
+//  Copyright (c) 2014 Andrei Zaharia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PostaMD/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PostaMD
+//
+//  Created by Andrei Zaharia on 2/28/14.
+//  Copyright (c) 2014 Andrei Zaharia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
